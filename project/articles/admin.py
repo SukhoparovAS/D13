609c6b4c8864df7ecd419525e33d41b5ae31f6ba
SukhoparovAS <sukhoparov.aleksandr@mail.ru>
2022-05-05 13:58:43 +0300

    main

--- project/articles/admin.py
+++ project/articles/admin.py
@@ -1,5 +1,23 @@
 from django.contrib import admin
 from .models import Article, Category
+from django import forms
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
 
-admin.site.register(Article)
+
+class ArticleAdminForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    form = ArticleAdminForm
+
+
+admin.site.register(Article, ArticleAdmin)
+
+
+# admin.site.register(Article)
 admin.site.register(Category)
